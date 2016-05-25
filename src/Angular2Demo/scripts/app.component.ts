11977f18693dd19392b43086dd36ab48069a949f
Shifdra <trevor166933@hotmail.com>
2016-05-25 13:18:45 -0400

    Committing VS project

---
+++ src/Angular2Demo/scripts/app.component.ts
@@ -0,0 +1,7 @@
+﻿import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular 2 Sample Application</h1>'
+})
+export class BaseComponent { }
+
---
+++ src/Angular2Demo/scripts/boot.ts
@@ -0,0 +1,3 @@
+﻿import {bootstrap}    from 'angular2/platform/browser'
+import {BaseComponent} from './app.component'
+bootstrap(BaseComponent);
